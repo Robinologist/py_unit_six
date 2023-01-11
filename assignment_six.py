ad3361a16966bcf5151ed70d44c65cbf25daf376
Robinologist <112644607+Robinologist@users.noreply.github.com>
2023-01-11 10:37:40 -0500

    Finished final assignment for Unit 6, a function that prints the line, word, and character count of a document

---
+++ assignment_six.py
@@ -0,0 +1,20 @@
+#Reads any file called "readme.txt" and returns the line, word, and character count
+
+f = open('readme.txt', 'r')                 #Open file
+contents = f.readlines()                    #Read lines in file
+f.close                                     #Close file
+lines=len(contents)                         #Assigns the number of lines to lines variable
+print("Lines:      ",lines)                 #Prints lines variable
+
+f = open('readme.txt', 'r')                 #Open file
+contents = f.read()                         #Read file
+f.close                                     #Close file
+contents = contents.split()                 #Splits the text into words in a list
+words=len(contents)                         #Assigns the number of words to words variable
+print("Words:      ",words)                 #Prints words variable
+
+f = open('readme.txt', 'r')                 #Open file
+contents = f.read()                         #Read file
+f.close                                     #Close file
+characters=len(contents)-lines+1            #The len function counts newline characters (\n) that are normally invisible, so by subtracting the number of lines and adding one I get only the number of visible characters. This does include spaces.
+print("Characters: ",characters)            #Prints character variable
